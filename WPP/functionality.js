15dbc848d49ff7e188d2e310c71ee1b7091e7d5e
Kuba Hofman <126712459+Mithynite@users.noreply.github.com>
2024-02-22 19:10:36 +0100

    Delete WPP directory

--- WPP/functionality.js
+++
@@ -1,8 +0,0 @@
-const hamburger = document.getElementById('hamburger');
-const menu = document.getElementById('menu');
-let on = false;
-hamburger.addEventListener('click', () => {
-    hamburger.classList.toggle('active');
-    menu.classList.toggle('active');
-    
-})
